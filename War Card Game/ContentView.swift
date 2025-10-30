5af643525e985028d67d8c429fe1c66f3e5e16c2
Conor Howard <47044266+conorhoward6510@users.noreply.github.com>
2025-10-30 23:18:12 +0000

    complete the V1 of the UI

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,13 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-cloth")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                HStack(spacing: 24) {
+                    Image("back")
+                        .resizable()
+                        .aspectRatio(64/89, contentMode: .fit)
+                        .frame(maxWidth: 160)
+
+                    Image("back")
+                        .resizable()
+                        .aspectRatio(64/89, contentMode: .fit)
+                        .frame(maxWidth: 160)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 24)
+                
+                Spacer()
+                
+                Button {
+                    print("Deal Pressed")
+                } label: {
+                    Image("button")
+                        .shadow(radius: 15)
+                }
+
+                Spacer()
+                
+                HStack(alignment: .firstTextBaseline, spacing: 40) {
+                    
+                    VStack {
+                        Text("Player")
+                            .bold()
+                        Text("0")
+                    }
+                    
+                    VStack {
+                        Text("CPU")
+                            .bold()
+                        Text("0")
+                    }
+                }
+                .foregroundStyle(.white)
+                .font(.largeTitle)
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
