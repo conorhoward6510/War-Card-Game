8c195b1821a249463ce158de2a9dfb04b2e81b94
Conor Howard <47044266+conorhoward6510@users.noreply.github.com>
2025-10-30 20:02:42 +0000

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Conor Howard on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
